408d50c8d65c69abcd1d0ac85ef4740d522167ce
Mar71nR <martinposadass66@gmail.com>
2023-10-18 19:33:44 -0700

    random word generator

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   createCanvas(400, 400);
+  
+    // Get a random element from an array using the random(Array) syntax
+let words = ['imminent', 'distance', 'handsome', 'toys', 'sparkle','sleep', 'addition', 'unsightly', 'chicken', 'hulking', 'pig' ,'abandoned'];
+let word = random(words); // select random word
+text(word, 10, 50); // draw the word
+describe(`word displayed at random. Either imminent, distance, handsome, toys, sparkle,sleep, addition, unsightly, chicken, hulking, pig, or abandoned`);
+  
 }
 
-function draw() {
-  background(220);
-}
